db6c213dc353b160d40999aabdde3ca29e3b223c
Trent Hashimoto <hashimotot@spu.edu>
2019-03-19 15:57:29 -0700

    Added scan method for NodeGroup This allows a NodeGroup to iterate through all of its nodes and add the dead nodes to a publicly accessible set

--- src/util/NodeGroup.java
+++ src/util/NodeGroup.java
@@ -1,6 +1,7 @@
 package util;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 import javafx.scene.shape.Circle;
 
@@ -11,11 +12,25 @@ import javafx.scene.shape.Circle;
 public class NodeGroup extends Circle {
 	public static final int DIAMETER = 10;
 	public ArrayList<Node> nodes;
+	public HashSet<Node> deadNodes;
 	
 	public NodeGroup(int x, int y) {
 		nodes = new ArrayList<Node>();
+		deadNodes = new HashSet<Node>();
 		this.setCenterX(x);
 		this.setCenterY(y);
 		this.resize(DIAMETER, DIAMETER);
+	}
+	
+	public void Scan() {
+		for(Node n : nodes) {
+			if(!n.online) {
+				deadNodes.add(n);
+				continue;
+			}
+			if(deadNodes.contains(n)) {
+				deadNodes.remove(n);
+			}
+		}
 	}
 }
